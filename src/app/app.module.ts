f9e1d5da608f848fd7eebb483b114d917616c276
Delgado2003 <joao.delgado@ekaizen.digital>
2022-03-18 11:39:44 -0300

    add component and the body of html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
 import {FormsModule} from "@angular/forms";
+import { ContactComponent } from './contact/contact.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageComponent
+    PageComponent,
+    ContactComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
